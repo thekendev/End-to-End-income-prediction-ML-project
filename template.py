23d536a5fa9e43ca23d1db9968f113df76190b1c
Kanayo Justice Kenechukwu <theken.dev@gmail.com>
2024-05-09 02:44:39 +0100

    project structure

---
+++ template.py
@@ -0,0 +1,72 @@
+import os
+import logging
+from pathlib import Path
+
+# Set up logging to both console and file
+log_folder = 'logs'
+os.makedirs(log_folder, exist_ok=True)  # Create logs folder if it doesn't exist
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:', handlers=[
+    logging.StreamHandler(),  # Output to console
+    logging.FileHandler(os.path.join(log_folder, 'logfile.log'))  # Output to file or log folder
+])
+
+
+project_name = "incomepredictor"
+
+list_of_files = [
+    # main folders
+    f"src/{project_name}/__init__.py",
+    f"research/__init__.py",
+
+    # src subfolders
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+
+    # src subfolders tree
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/config_entity.py",
+
+
+
+    # yaml files/folders
+    "params.yaml",
+    "schema.yaml",
+    "config/config.yaml",
+
+    # .py files
+    "setup.py",
+    "app.py",
+    "main.py",
+
+    #other files and folders
+    "Dockerfile.py",
+    "requirements.txt",
+    "templates/index.html",
+    
+
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Created directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Created file: {filepath}")
+
+    else:
+        logging.info(f"File: {filepath} already exists")
+
