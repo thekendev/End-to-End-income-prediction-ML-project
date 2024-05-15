6244af3b1b5870cb365b0a128e04950a0ae9ee58
Kanayo Justice Kenechukwu <theken.dev@gmail.com>
2024-05-15 16:35:46 +0100

    data validation done

--- incomepredictor/src/config/configuration.py
+++ incomepredictor/src/config/configuration.py
@@ -1,7 +1,8 @@
 #Update the configuration manager in src config for data ingestion
 from src.constants import *
 from incomepredictor.src.utils.common import read_yaml, create_directories
-from src.entity.config_entity import (DataIngestionConfig)
+from src.entity.config_entity import (DataIngestionConfig,
+                                      DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -31,3 +32,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from incomepredictor.src.logging import logger
 from incomepredictor.src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from incomepredictor.src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 try:
@@ -8,6 +8,18 @@ try:
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
         raise e
